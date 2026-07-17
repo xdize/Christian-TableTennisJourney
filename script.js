0b31be21be273260c68aa86b33d055825405fa0a
xdize <creezixdavid@gmail.com>
2026-07-18 01:25:39 +0800

    rollback

--- script.js
+++ script.js
@@ -1,23 +1,3 @@
-const button = document.getElementById("showMore");
-const hiddenPhotos = document.querySelectorAll(".hidden-photo");
-
-let expanded = false;
-
-button.addEventListener("click", () => {
-
-    expanded = !expanded;
-
-    hiddenPhotos.forEach(photo => {
-
-        photo.style.display = expanded ? "block" : "none";
-
-    });
-
-    button.textContent = expanded ? "Show Less" : "Show More";
-
-});
-
-
 // ===============================
 // Gallery Lightbox
 // ===============================
